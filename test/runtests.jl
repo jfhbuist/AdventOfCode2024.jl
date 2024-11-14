e2b0450c54bf29b3a1290d225d832d15bd6ec290
Jurriaan Buist <37274013+jfhbuist@users.noreply.github.com>
2024-11-14 23:24:48 +0100

    fix test

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@ using AdventOfCode2024
 using Test
 
 @testset "day_1_2022" begin
-    input = "input_test/day_1_2022.txt";
+    input = "../input_test/day_1_2022.txt";
     test_answer_part_1 = 24000
     test_answer_part_2 = 45000
     @test main_day_1_2022(input, 1) == test_answer_part_1
